a39eccb276c6526535da4b509b16cb1504b0355e
9qUmV4 <9qumv4@gmail.com>
2022-05-19 13:18:23 +0200

    Initial commit

---
+++ config-backup/core/application.py
@@ -0,0 +1,11 @@
+
+
+class Application:
+    """
+    Class for handeling paths and backups for one service on differen machines.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    
---
+++ config-backup/core/config.py
@@ -0,0 +1,7 @@
+# Handles the import of config files
+
+import pathlib
+
+DEFAULT_CONFIG_PATH = pathlib.Path(__file__).parent / '../default-config.yml'
+DEFAULT_USER_CONFIG_PATH = pathlib.Path(__file__).parent / '../config.yml'
+
